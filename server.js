ad4bcd1e4e77e77c0e949373ae268b64129e1b27
Trung <ungkientrung@gmail.com>
2019-06-13 17:52:54 +0300

    try other heroku script

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+const PORT = process.env.PORT || 5000;
 
 // Connect Database
 connectDB();
@@ -16,14 +17,22 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
-// Serve static assets in production
-if (process.env.NODE_DEV === 'production') {
-  //set statis folder
-  app.use(express.static('client/build'));
+// // Serve static assets in production
+// if (process.env.NODE_DEV === 'production') {
+//   //set statis folder
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
